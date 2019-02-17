1034c6325db18bd6eae07f26d8d02aef8207cfb7
Julian <julian@rega-sense.ch>
2019-02-18 00:23:27 +0100

    change port in server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express')
 const ejs = require('ejs')
 const bodyParser = require('body-parser')
 const fs = require('fs')
+const port = process.env.PORT || 3000;
 var app = express()
 
 /** bodyParser.urlencoded(options)
